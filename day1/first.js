8c5f4f6f2ce4d3a4811100e3894f55018a8d898f
nikitkushwaha448 <nikitkushwaha448@gmail.com>
2025-06-10 12:30:13 +0530

    day 1

---
+++ day1/first.js
@@ -0,0 +1,13 @@
+const header1 = document.createElement('h1');
+header1.innerText = "Hello coder army";
+header1.style.backgroundColor = "blue";
+header1.color = "bLack";
+
+header1.style.fontSize = "30px";
+
+header1.style.color = "black";
+
+
+const root = document.getElementById('root');
+root.append(header1);
+
